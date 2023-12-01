0e287d27b07c7b3e9b5d953f4c9a63d3ff3fe8bb
Igor Belov <bisgarik@gmail.com>
2023-12-02 00:11:57 +0100

    add keyboard with arrows

--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -6,6 +6,16 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 calalog_list = InlineKeyboardBuilder()
 calalog_list.row(types.InlineKeyboardButton(text="🥟 Пельмени", callback_data="dumplings"))
 calalog_list.row(types.InlineKeyboardButton(text="🍲 Супы", callback_data="soups"))
+# calalog_list.row(types.InlineKeyboardButton(text="Котлеты", callback_data="cutlets"))
 calalog_list.row(types.InlineKeyboardButton(text="🥗 Салаты", callback_data="salads"))
 calalog_list.row(types.InlineKeyboardButton(text="🫙 Соусы", callback_data="sauces"))
 
+
+# Кл
+def get_keyboard(index: int):
+    keyboard = InlineKeyboardBuilder()
+    keyboard.add(types.InlineKeyboardButton(text="⬅️", callback_data=f"prev_{index}"))
+    keyboard.add(types.InlineKeyboardButton(text="➡️", callback_data=f"next_{index}"))
+    keyboard.row(types.InlineKeyboardButton(text="Заказать", callback_data="order"),
+                 types.InlineKeyboardButton(text="Назад", callback_data="back"))
+    return keyboard
